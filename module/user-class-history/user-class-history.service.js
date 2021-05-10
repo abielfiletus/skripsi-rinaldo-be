dde9c07f1b1ace82c66eaa332aa0917b42db7586
abiel filetus <abiel@terralogiq.com>
2021-05-10 15:49:38 +0700

    [UPDATE] Error Handling

--- module/user-class-history/user-class-history.service.js
+++ module/user-class-history/user-class-history.service.js
@@ -7,15 +7,15 @@ class UserClassHistoryService {
     const where = []
 
     if (body.form['class_id']) {
-      where.push(`uch.class_id = ${body.form['class_id']}`)
+      where.push(`uch."class_id" = ${body.form['class_id']}`)
     }
 
     if (body.form['user_id']) {
-      where.push(`uch.user_id = ${body.form['user_id']}`)
+      where.push(`uch."user_id" = ${body.form['user_id']}`)
     }
 
     if (body.form['start']) {
-      where.push(`uch.createdAt BETWEEN "${body.form['start']}" AND "${body.form['end']}"`)
+      where.push(`uch."createdAt" BETWEEN "${body.form['start']}" AND "${body.form['end']}"`)
     }
 
     return await model.sequelize.query(`SELECT DISTINCT uch.id, uch.status, uch.nilai, uch.durasi, uch.class_materi_id, cm.name as class_materi_name FROM user_class_history uch JOIN class_materi cm ON uch.class_materi_id = cm.id ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''} ORDER BY uch.class_materi_id ASC`)
