da4dbb7f81e5511925f8406f163c3d769ff563ce
abiel filetus <abiel@terralogiq.com>
2021-05-10 09:10:44 +0700

    [UPDATE] Response

--- module/class-materi/class-materi.service.js
+++ module/class-materi/class-materi.service.js
@@ -17,15 +17,7 @@ class ClassMateriService {
       where += `cm.class_id = "${body.form['name']}"`
     }
 
-    // const offset = body.offset ? parseInt(body.offset) : 0
-    // const limit = body.length && body.length > 0 ? parseInt(body.length) : 10000000
-    // const order = [body.order ? body.order : ['id', 'ASC']]
-    // const include = {
-    //   model: classQuizModel,
-    //   as: 'quiz',
-    // }
-
-    return await model.sequelize.query(`SELECT cm.id, cm.name, cm.path, cm.class_id, c.name as class_name, cq.id as quiz_id, cq.name as quiz_name, cq.total_soal as quiz_total_soal, cq.nilai_lulus as quiz_nilai_lulus, cq.class_materi_id, uch.status FROM class_materi cm JOIN class c ON cm.class_id = c.id LEFT OUTER JOIN class_quiz cq ON cm.id = cq.class_materi_id LEFT OUTER JOIN user_class_history uch ON cq.id = uch.class_id AND uch.user_id = ${body.form['user_id']} ${where}`)[0];
+    return await model.sequelize.query(`SELECT cm.id, cm.name, cm.path, cm.class_id, c.name as class_name, cq.id as quiz_id, cq.name as quiz_name, cq.total_soal as quiz_total_soal, cq.nilai_lulus as quiz_nilai_lulus, cq.class_materi_id, uch.status FROM class_materi cm JOIN class c ON cm.class_id = c.id LEFT OUTER JOIN class_quiz cq ON cm.id = cq.class_materi_id LEFT OUTER JOIN user_class_history uch ON cq.id = uch.class_id AND uch.user_id = ${body.form['user_id']} ${where}`);
   }
 
   async getOne(id) {
