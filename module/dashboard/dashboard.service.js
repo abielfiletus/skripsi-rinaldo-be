bec11d5a7c05ce511ecdb149518412215833514c
abiel filetus <abiel@terralogiq.com>
2021-05-10 13:09:18 +0700

    [UPDATE] Remove duplicate data

--- module/dashboard/dashboard.service.js
+++ module/dashboard/dashboard.service.js
@@ -13,7 +13,7 @@ class DashboardService {
       where.push(`class_id = ${body.class_id}`)
     }
 
-    return await model.sequelize.query(`select sum(nilai) as nilai, sum(durasi) as durasi, (CASE WHEN sum(nilai) is null THEN null WHEN (select nilai_lulus from class where id = ${body.class_id}) < sum(nilai) THEN 'Lulus' ELSE 'Tidak Lulus' END) as status from user_class_history ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''}`)
+    return await model.sequelize.query(`uc.id, sum(uch.nilai) as nilai, sum(uch.durasi) as durasi, (CASE WHEN sum(uch.nilai) is null THEN null WHEN (select nilai_lulus from class where id = ${body.class_id}) < sum(uch.nilai) THEN 'Lulus' ELSE 'Tidak Lulus' END) as status from user_class uc LEFT JOIN user_class_history uch on uch.user_id = uc.id ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''}`)
   }
 
 }
