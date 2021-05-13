8aa83644a6deeb4054dc8e48289e3b7215430359
abiel filetus <abiel@terralogiq.com>
2021-05-13 19:24:49 +0700

    [FIX] Change double quotes to single qou

--- module/class/class.service.js
+++ module/class/class.service.js
@@ -11,23 +11,23 @@ class ClassService {
     const where = []
 
     if (body.form['name']) {
-      where.push(`name iLike '%${body.form['name']}%'`)
+      where.push(`c.name iLike '%${body.form['name']}%'`)
     }
 
     if (body.form['user_id']) {
-      where.push(`createdBy = '${body.form['user_id']}'`)
+      where.push(`c.createdBy = '${body.form['user_id']}'`)
     }
 
     if (body.form['code']) {
-      where.push(`code = '${body.form['code']}'`)
+      where.push(`c.code = '${body.form['code']}'`)
     }
 
     if (body.form['start']) {
-      where.push(`start >= '${body.form['start']}'`)
+      where.push(`c.start >= '${body.form['start']}'`)
     }
 
     if (body.form['end']) {
-      where.push(`end <= '${body.form['end']}'`)
+      where.push(`c.end <= '${body.form['end']}'`)
     }
 
     return classModel.sequelize.query(`SELECT c.id, c.name, c.code, c.end, c.start, c.nilai_lulus, count(DISTINCT uc.id) as student_total, count(DISTINCT cm.id) as materi_total FROM class c LEFT OUTER JOIN user_class uc ON c.id = uc.class_id LEFT OUTER JOIN class_materi cm ON c.id = cm.class_id ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''} GROUP BY 1 `)
