d243d2d9bcea8d41d68fda9dc8f69c847385178a
abiel filetus <abiel@terralogiq.com>
2021-05-17 18:10:44 +0700

    [UPDATE] Response

--- module/meeting/meeting.service.js
+++ module/meeting/meeting.service.js
@@ -1,3 +1,5 @@
+const usulanMeetingModel = require('../usulan-meeting/usulan-meeting.model')
+const classModel = require('../class/class.model')
 const model = require('./meeting.model')
 
 class MeetingService {
@@ -10,11 +12,23 @@ class MeetingService {
       where['class_id'] = body.form['class_id']
     }
 
+    if (body.form['user_id']) {
+      where['$usulan_meeting.class.createdBy$'] = body.form['user_id']
+    }
+
     const offset = body.offset ? parseInt(body.offset) : 0
     const limit = body.length && body.length > 0 ? parseInt(body.length) : 10000000
     const order = [body.order ? body.order : ['id', 'ASC']]
+    const include = {
+      model: usulanMeetingModel,
+      as: 'usulan_meeting',
+      include: {
+        model: classModel,
+        as: 'class'
+      }
+    }
 
-    return await model.findAll({where, offset, limit, order})
+    return await model.findAll({where, offset, limit, order, include})
   }
 
   async getOne(id, raw=false) {
