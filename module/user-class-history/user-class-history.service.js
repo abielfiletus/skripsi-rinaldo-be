a19bb2b6927e57d4ca83f5d5a3d4404acda9ef00
abiel filetus <abiel@terralogiq.com>
2021-05-18 10:26:09 +0700

    [UPDATE] Response

--- module/user-class-history/user-class-history.service.js
+++ module/user-class-history/user-class-history.service.js
@@ -18,7 +18,11 @@ class UserClassHistoryService {
       where.push(`uch."createdAt" BETWEEN '${body.form['start']}' AND '${body.form['end']}'`)
     }
 
-    return await model.sequelize.query(`SELECT DISTINCT uch.id, uch.status, uch.nilai, uch.durasi, uch.class_materi_id, cm.name as class_materi_name FROM user_class_history uch JOIN class_materi cm ON uch.class_materi_id = cm.id ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''} ORDER BY uch.class_materi_id ASC`)
+    if (body.form['class_materi_id']) {
+      where.push(`uch."class_materi_id" = ${body.form['class_materi_id']}`)
+    }
+
+    return await model.sequelize.query(`SELECT DISTINCT uch.id, uch.status, uch.nilai, uch.durasi, uch.class_materi_id, cm.name as class_materi_name, u.name, u.avatar FROM user_class_history uch JOIN class_materi cm ON uch.class_materi_id = cm.id JOIN "user" u ON uch.user_id = u.id ${where.length > 0 ? 'WHERE ' + where.join(' AND ') : ''} ORDER BY uch.class_materi_id`)
   }
 
   async getOne(id, raw=false) {
